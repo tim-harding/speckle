f5a76342999d7d5e3ad6606d96d294afa43f4a4d
Timothy Harding <timothy@harding.vip>
2026-05-24 16:17:18 -0700

    Add the remaining branches

--- speckle-macro/src/lib.rs
+++ speckle-macro/src/lib.rs
@@ -1,8 +1,8 @@
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{
-    ItemConst, ItemEnum, ItemFn, ItemForeignMod, ItemImpl, ItemMacro, ItemMod, ItemStatic,
-    ItemStruct, ItemTrait, ItemUnion,
+    ItemConst, ItemEnum, ItemFn, ItemImpl, ItemMacro, ItemMod, ItemStatic, ItemStruct, ItemTrait,
+    ItemUnion,
     parse::{Parse, ParseStream, Result as ParseResult},
     parse_macro_input,
 };
@@ -18,15 +18,32 @@ enum Item {
     Trait(ItemTrait),
     Impl(ItemImpl),
     Macro(ItemMacro),
-    ForeignMod(ItemForeignMod),
     Mod(ItemMod),
 }
 
 impl Parse for Item {
     fn parse(input: ParseStream) -> ParseResult<Self> {
         let lookahead = input.lookahead1();
-        if lookahead.peek(syn::token::Fn) {
+        if lookahead.peek(syn::token::Static) {
+            Ok(Item::Static(input.parse::<ItemStatic>()?))
+        } else if lookahead.peek(syn::token::Const) {
+            Ok(Item::Const(input.parse::<ItemConst>()?))
+        } else if lookahead.peek(syn::token::Struct) {
+            Ok(Item::Struct(input.parse::<ItemStruct>()?))
+        } else if lookahead.peek(syn::token::Enum) {
+            Ok(Item::Enum(input.parse::<ItemEnum>()?))
+        } else if lookahead.peek(syn::token::Union) {
+            Ok(Item::Union(input.parse::<ItemUnion>()?))
+        } else if lookahead.peek(syn::token::Fn) {
             Ok(Item::Fn(input.parse::<ItemFn>()?))
+        } else if lookahead.peek(syn::token::Trait) {
+            Ok(Item::Trait(input.parse::<ItemTrait>()?))
+        } else if lookahead.peek(syn::token::Impl) {
+            Ok(Item::Impl(input.parse::<ItemImpl>()?))
+        } else if lookahead.peek(syn::token::Macro) {
+            Ok(Item::Macro(input.parse::<ItemMacro>()?))
+        } else if lookahead.peek(syn::token::Mod) {
+            Ok(Item::Mod(input.parse::<ItemMod>()?))
         } else {
             Err(lookahead.error())
         }
@@ -59,7 +76,6 @@ pub fn speckle(_args: TokenStream, input: TokenStream) -> TokenStream {
         Item::Trait(_) => todo!(),
         Item::Impl(_) => todo!(),
         Item::Macro(_) => todo!(),
-        Item::ForeignMod(_) => todo!(),
         Item::Mod(_) => todo!(),
     }
 }
