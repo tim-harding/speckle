261d39df07cc858ac4de37e59404bd4291e656ef
Timothy Harding <timothy@harding.vip>
2026-05-24 11:56:37 -0700

    Speckle macro

--- speckle-macro/src/lib.rs
+++ speckle-macro/src/lib.rs
@@ -1,1 +1,20 @@
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{ItemFn, parse_macro_input};
 
+#[proc_macro_attribute]
+pub fn speckle(_args: TokenStream, input: TokenStream) -> TokenStream {
+    let ItemFn {
+        attrs,
+        vis,
+        sig,
+        block: _,
+    } = parse_macro_input!(input as ItemFn);
+
+    TokenStream::from(quote! {
+        #(#attrs)*
+        #vis #sig {
+            todo!()
+        }
+    })
+}
